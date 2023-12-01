7d8962c9b00468fba54f3314feac8f092089ae9f
h3rby7 <herbielutz@gmx.net>
2023-12-01 19:12:27 +0100

    first code draft to send actual http request

--- main/rest/request.c
+++ main/rest/request.c
@@ -2,6 +2,7 @@
 #include "request.h"
 #include "esp_log.h"
 #include <stdint.h>
+#include "esp_http_client.h"
 
 static const char *REST_REQ_TAG = "REST";
 
@@ -13,7 +14,40 @@ static const char *REST_REQ_TAG = "REST";
  */
 int send_rest_request()
 {
+  ESP_LOGV(REST_REQ_TAG, "Getting UID...");
   uint64_t ownID = get_unique_id();
-  ESP_LOGW(REST_REQ_TAG, "Would send REST with ID %llu", ownID);
+  ESP_LOGD(REST_REQ_TAG, "UID acquired -> '%llu'", ownID);
+
+  ESP_LOGV(REST_REQ_TAG, "Initializing HTTP Client...");
+  esp_http_client_config_t config = {
+      .host = CONFIG_DMX_WEB_SERVER,
+      .path = "/api/v1/trigger",
+      .method = HTTP_METHOD_POST,
+      .transport_type = HTTP_TRANSPORT_OVER_TCP,
+  };
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+  ESP_LOGD(REST_REQ_TAG, "HTTP Client initialized.");
+
+  // UID as POST content
+  char uid[20];
+  sprintf(uid, "%llu", ownID);
+  esp_http_client_set_post_field(client, uid, strlen(uid));
+
+  ESP_LOGI(REST_REQ_TAG, "Performing POST to '%s', using path '%s' with content '%s'", config.host, config.path, uid);
+  esp_err_t err = esp_http_client_perform(client);
+  if (err == ESP_OK)
+  {
+    ESP_LOGD(REST_REQ_TAG, "HTTP POST Status = %d, content_length = %" PRId64,
+             esp_http_client_get_status_code(client),
+             esp_http_client_get_content_length(client));
+  }
+  else
+  {
+    ESP_LOGE(REST_REQ_TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+  }
+
+  ESP_LOGV(REST_REQ_TAG, "Cleaning up HTTP Client...");
+  esp_http_client_cleanup(client);
+  ESP_LOGD(REST_REQ_TAG, "HTTP Client cleaned up.");
   return 1;
 }
